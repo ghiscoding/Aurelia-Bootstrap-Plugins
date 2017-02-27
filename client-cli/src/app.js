f78897a92d249b5a421f5f5602e1b1f581d5ceb6
Ghislain B <gbeaulac@gmail.com>
2017-02-26 22:46:58 -0500

    Renamed aba filenames to abp to represent plugins

--- client-cli/src/app.js
+++ client-cli/src/app.js
@@ -2,7 +2,7 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Aurelia';
     config.map([
-      { route: 'bootstrap-plugins', name: 'bootstrap-plugins', moduleId: './bootstrap-plugins', nav: true, title: 'Bootstrap-plugins' },
+      { route: 'bootstrap-plugins', name: 'bootstrap-plugins', moduleId: './bootstrap-plugins', nav: true, title: 'Bootstrap Plugins' },
       { route: '', redirect: 'bootstrap-plugins' }
     ]);
 
