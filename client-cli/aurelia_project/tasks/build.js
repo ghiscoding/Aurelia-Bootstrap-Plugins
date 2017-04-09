90a988bac59813343527f0fda08a309cd9b12ebd
Ghislain B <gbeaulac@gmail.com>
2017-04-09 01:41:08 -0400

    aurelia-bootstrap-select fix client-cli sample

--- client-cli/aurelia_project/tasks/build.js
+++ client-cli/aurelia_project/tasks/build.js
@@ -2,6 +2,7 @@ import gulp from 'gulp';
 import transpile from './transpile';
 import processMarkup from './process-markup';
 import processCSS from './process-css';
+import copyFiles from './copy-files';
 import {build} from 'aurelia-cli';
 import project from '../aurelia.json';
 import prepareFontAwesome from './prepare-font-awesome';
@@ -12,6 +13,7 @@ export default gulp.series(
     transpile,
     processMarkup,
     processCSS,
+    copyFiles,
     prepareFontAwesome
   ),
   writeBundles
