c038ddcdf0d98661011d3ea864d46d847e728894
Ghislain B <gbeaulac@gmail.com>
2017-04-09 22:56:32 -0400

    Remove unecessary dev files

--- client-cli/src/resources/index.js
+++ client-cli/src/resources/index.js
@@ -1,8 +1,5 @@
 export function configure(config) {
   config.globalResources([
-    './elements/bselect',
-    './elements/bootstrap-select',
-    //'./behaviors/optional.js',
     './value-converters/stringify'
   ]);
 }
