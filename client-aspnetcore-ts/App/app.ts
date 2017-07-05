10fca485f05691317063ae95d6450fcaf08098ad
Ghislain B <gbeaulac@gmail.com>
2017-07-05 00:42:13 -0400

    trying to fix ASP.Net client example

--- client-aspnetcore-ts/App/app.ts
+++ client-aspnetcore-ts/App/app.ts
@@ -2,14 +2,14 @@ import {Router, RouterConfiguration} from 'aurelia-router';
 import {PLATFORM} from 'aurelia-pal';
 
 export class App {
-  public router: Router;
+  router: Router;
 
-  public configureRouter(config: RouterConfiguration, router: Router) {
+  configureRouter(config: RouterConfiguration, router: Router) {
     config.title = 'Aurelia';
     config.map([
-      { route: PLATFORM.moduleName('bootstrap-plugins'), name: 'bootstrap-plugins', moduleId: './bootstrap-plugins', nav: true, title: 'Bootstrap Plugins' },
-      { route: PLATFORM.moduleName('validation-form'), name: 'validation-form', moduleId: './validation-form', nav: true, title: 'Validation Form' },
-      { route: '', redirect: 'validation-form' }
+      { route: 'bootstrap-plugins', name: 'bootstrap-plugins',  moduleId: PLATFORM.moduleName('./bootstrap-plugins'), nav: true, title: 'Bootstrap Plugins' },
+      { route: 'validation-form',   name: 'validation-form',    moduleId: PLATFORM.moduleName('./validation-form'),   nav: true, title: 'Validation Form' },
+      { route: '', redirect: PLATFORM.moduleName('validation-form') }
     ]);
 
     this.router = router;
--- client-aspnetcore-ts/webpack.config.js
+++ client-aspnetcore-ts/webpack.config.js
@@ -1,67 +1,121 @@
-const path = require("path");
-const { AureliaPlugin } = require("aurelia-webpack-plugin");
-const ProvidePlugin = require('webpack/lib/ProvidePlugin');
-const ContextReplacementPlugin = require('webpack/lib/ContextReplacementPlugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const { AureliaPlugin } = require('aurelia-webpack-plugin');
+const { optimize: { CommonsChunkPlugin }, ProvidePlugin } = require('webpack')
+const { TsConfigPathsPlugin, CheckerPlugin } = require('awesome-typescript-loader');
 
-module.exports = {
-  entry: { main: "aurelia-bootstrapper" },
+// config helpers:
+const ensureArray = (config) => config && (Array.isArray(config) ? config : [config]) || []
+const when = (condition, config, negativeConfig) =>
+  condition ? ensureArray(config) : ensureArray(negativeConfig)
 
-  output: {
-    path: path.join(__dirname, "wwwroot", "dist"),
-    filename: "app.js",
-    publicPath: "/dist/",
-  },
+// primary config:
+const title = 'Aurelia Navigation Skeleton';
+const outDir = path.resolve(__dirname, 'dist');
+const srcDir = path.resolve(__dirname, 'src');
+const nodeModulesDir = path.resolve(__dirname, 'node_modules');
+const baseUrl = '/';
 
+const cssRules = [
+  { loader: 'css-loader' },
+  {
+    loader: 'postcss-loader',
+    options: { plugins: () => [require('autoprefixer')({ browsers: ['last 2 versions'] })]}
+  }
+]
+
+module.exports = ({production, server, extractCss, coverage} = {}) => ({
   resolve: {
-    alias: {
-        // Force all modules to use the same jquery version.
-        'jquery': path.join(__dirname, 'node_modules/jquery/src/jquery')
-    },
-    extensions: [".ts", ".js"],
-    modules: ["App", "node_modules"],    
+    extensions: ['.ts', '.js'],
+    modules: [srcDir, 'node_modules'],
   },
-
+  entry: {
+    app: ['aurelia-bootstrapper'],
+    vendor: ['bluebird', 'jquery', 'bootstrap'],
+  },
+  output: {
+    path: outDir,
+    publicPath: baseUrl,
+    filename: production ? '[name].[chunkhash].bundle.js' : '[name].[hash].bundle.js',
+    sourceMapFilename: production ? '[name].[chunkhash].bundle.map' : '[name].[hash].bundle.map',
+    chunkFilename: production ? '[name].[chunkhash].chunk.js' : '[name].[hash].chunk.js',
+  },
+  devServer: {
+    contentBase: outDir,
+    // serve index.html for all 404 (required for push-state)
+    historyApiFallback: true,
+  },
   module: {
     rules: [
+      // CSS required in JS/TS files should use the style-loader that auto-injects it into the website
+      // only when the issuer is a .js/.ts file, so the loaders are not applied inside html templates
       {
         test: /\.css$/i,
-        loader: 'css-loader',
-        issuer: /\.html?$/i
+        issuer: [{ not: [{ test: /\.html$/i }] }],
+        use: extractCss ? ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: cssRules,
+        }) : ['style-loader', ...cssRules],
       },
       {
         test: /\.css$/i,
-        loader: ['style-loader', 'css-loader'],
-        issuer: /\.[tj]s$/i
+        issuer: [{ test: /\.html$/i }],
+        // CSS required in templates cannot be extracted safely
+        // because Aurelia would try to require it again in runtime
+        use: cssRules,
       },
-      { test: /\.html$/i, loaders: "html-loader" },
-      { test: /\.ts$/i, loaders: "ts-loader" },
+      { test: /\.html$/i, loader: 'html-loader' },
+      { test: /\.ts$/i, loader: 'awesome-typescript-loader', exclude: nodeModulesDir },
+      { test: /\.json$/i, loader: 'json-loader' },
       // use Bluebird as the global Promise implementation:
       { test: /[\/\\]node_modules[\/\\]bluebird[\/\\].+\.js$/, loader: 'expose-loader?Promise' },
       // exposes jQuery globally as $ and as jQuery:
       { test: require.resolve('jquery'), loader: 'expose-loader?$!expose-loader?jQuery' },
-      {
-        test: /\.png$/,
-        loader: 'url-loader?limit=100000'
-      },
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
-      },
-      {
-        test: /\.(ttf|otf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?|(jpg|gif)$/,
-        loader: 'file-loader'
-      },
+      // embed small images and fonts as Data Urls and larger ones as files:
+      { test: /\.(png|gif|jpg|cur)$/i, loader: 'url-loader', options: { limit: 8192 } },
+      { test: /\.woff2(\?v=[0-9]\.[0-9]\.[0-9])?$/i, loader: 'url-loader', options: { limit: 10000, mimetype: 'application/font-woff2' } },
+      { test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/i, loader: 'url-loader', options: { limit: 10000, mimetype: 'application/font-woff' } },
+      // load these fonts normally, as files:
+      { test: /\.(ttf|eot|svg|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/i, loader: 'file-loader' },
+      ...when(coverage, {
+        test: /\.[jt]s$/i, loader: 'istanbul-instrumenter-loader',
+        include: srcDir, exclude: [/\.{spec,test}\.[jt]s$/i],
+        enforce: 'post', options: { esModules: true },
+      })
     ]
   },
-
   plugins: [
     new AureliaPlugin(),
-    new ContextReplacementPlugin(/moment[\/\\]locale$/, /en|fr/),
     new ProvidePlugin({
       'Promise': 'bluebird',
       '$': 'jquery',
       'jQuery': 'jquery',
       'window.jQuery': 'jquery',
-    })
-  ]
-};
+    }),
+    new TsConfigPathsPlugin(),
+    new CheckerPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.ejs',
+      minify: production ? {
+        removeComments: true,
+        collapseWhitespace: true
+      } : undefined,
+      metadata: {
+        // available in index.ejs //
+        title, server, baseUrl
+      },
+    }),
+    new CopyWebpackPlugin([
+      { from: 'static/favicon.ico', to: 'favicon.ico' }
+    ]),
+    ...when(extractCss, new ExtractTextPlugin({
+      filename: production ? '[contenthash].css' : '[id].css',
+      allChunks: true,
+    })),
+    ...when(production, new CommonsChunkPlugin({
+      name: 'common'
+    }))
+  ],
+})
