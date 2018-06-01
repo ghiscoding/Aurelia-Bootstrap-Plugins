dda92718ec6df7a7e08351422b4aeb877e06f3f6
Moritz Becker <moritz.becker@gmx.at>
2018-06-01 11:57:45 +0200

    fix(select) cleanup, fix endless recursion on selection updates

--- aurelia-bootstrap-select/src/abp-select.js
+++ aurelia-bootstrap-select/src/abp-select.js
@@ -301,19 +301,16 @@ export class AbpSelectCustomElement {
 
   /**
    * Find item(s) in the collection.
-   * To support the "multiple" selection attribute and to make our life and code easier, we will use arrays to search and return the found item(s).
-   * The array advantage is that even if we are not using "multiple", we will still have 1 value at the end (the only difference is that it's inside an array).
    * @param {array} collection
    * @param {any} newValue
    * @param {string} objectKey
-   * @return {object} found selection, the output is an object with structure of selection = { indexes: [], items: [] };
+   * @return {object} found selection, the output is an object with structure of selection = { indexes: any | any[], items: any | any[] };
    */
   findItems(collection, newValue, objectKey) {
-    let foundItems = [];
     let searchingItems = [];
     let selection = {
-      indexes: [],
-      items: []
+      index: this.multiple ? [] : undefined,
+      item: this.multiple ? [] : undefined
     };
     if (newValue === null || newValue === undefined) {
       return selection;
@@ -332,9 +329,15 @@ export class AbpSelectCustomElement {
         return this.util.isObject(item) ? item[objectKey] == searchFilter : item == searchFilter;
       });
       if (foundItem) {
-        selection.indexes.push(this.util.isObject(foundItem) ? foundItem[objectKey] : foundItem);
-        selection.items.push(foundItem);
-        foundItems.push(foundItem);
+        const foundItemIndex = this.util.isObject(foundItem) ? foundItem[objectKey] : foundItem;
+        if (this.multiple) {
+          selection.index.push(foundItemIndex);
+          selection.item.push(foundItem);
+        } else {
+          selection.index = foundItemIndex;
+          selection.item = foundItem;
+          break;
+        }
       }
     }
 
@@ -350,19 +353,11 @@ export class AbpSelectCustomElement {
     if (!selection) {
       return true;
     }
-    return selection.items.length === 0 && selection.indexes.length === 0;
-  }
-
-  /**
-   * From the selection object provided, we want to know if the selection is empty
-   * The structure is:: selection = { indexes: [], items: [] };
-   * @param {obejct} selection object
-   */
-  isNotEmptySelection(selection) {
-    if (!selection) {
-      return true;
+    if (this.multiple) {
+      return selection.item.length === 0;
+    } else {
+      return selection.item ? false : true;
     }
-    return selection.items.length > 0 && selection.indexes.length > 0;
   }
 
   /**
@@ -372,10 +367,10 @@ export class AbpSelectCustomElement {
    */
   renderSelection(selection) {
     if (this.domElm) {
-      if (selection.indexes.length > 0) {
-        this.domElm.selectpicker('val', selection.indexes);
-      } else if (this.util.parseBool(this.emptyOnNull) && this.isEmptySelection(selection)) {
+      if (this.isEmptySelection(selection) && this.util.parseBool(this.emptyOnNull)) {
         this.domElm.selectpicker('val', null);
+      } else if (!this.isEmptySelection(selection)) {
+        this.domElm.selectpicker('val', selection.index);
       }
     }
   }
@@ -391,15 +386,11 @@ export class AbpSelectCustomElement {
       let selection = this.findItems(this.collection, newValue || this._originalSelectedIndexes, this.objectKey);
 
       // get selected indexes (ids), unless user chose to emptyOnNull on first pass
-      if ((!this.util.parseBool(this.emptyOnNull) && !this.multiple) || this.isNotEmptySelection(selection)) {
-        if (selection.indexes.length > 0) {
-          const selectedValue = selection.indexes;
-          this.selectedValue = (!this.multiple && Array.isArray(selectedValue)) ? selectedValue[0] : selectedValue;
-        } else {
-          // value could be an object, if so we will use the objectKey (object.id by default)
-          const selectedValue = (this.util.isObject(this.collection[0]) ? this.collection[0][this.objectKey] : this.collection[0]);
-          this.selectedValue = (!this.multiple && Array.isArray(selectedValue)) ? selectedValue[0] : selectedValue;
-        }
+      if (this.isEmptySelection(selection) && !this.util.parseBool(this.emptyOnNull) && !this.multiple) {
+        // value could be an object, if so we will use the objectKey (object.id by default)
+        this.selectedValue = (this.util.isObject(this.collection[0]) ? this.collection[0][this.objectKey] : this.collection[0]);
+      } else if (!this.isEmptySelection(selection)) {
+        this.selectedValue = selection.index;
       }
 
       this.renderSelection(selection);
@@ -415,17 +406,7 @@ export class AbpSelectCustomElement {
   selectedValueChanged(newValue, oldValue) {
     if (!this.util.isEqual(newValue, oldValue)) {
       let selection = this.findItems(this.collection, newValue || this._originalSelectedObjects, this.objectKey);
-
-      // get selected items (string/object), unless user chose to emptyOnNull on first pass
-      if ((!this.util.parseBool(this.emptyOnNull) && !this.multiple) || this.isNotEmptySelection(selection)) {
-        const selectedItem = (selection.items.length > 0) ? selection.items : this.collection[0];
-        this.selectedItem = (!this.multiple && Array.isArray(selectedItem)) ? selectedItem[0] : selectedItem;
-      } else if (this.util.parseBool(this.emptyOnNull) && !this.multiple) {
-        // if user has defined emptyOnNull and is not set to multiple, we should set the item to undefined
-        this.selectedItem = undefined;
-      }
-
-      this.renderSelection(selection);
+      this.selectedItem = selection.item;
     }
   }
 
@@ -438,12 +419,13 @@ export class AbpSelectCustomElement {
     this.domElm.on('loaded.bs.select', (e) => {
       let newValue = this._originalSelectedIndexes || this._originalSelectedObjects;
       let selection = this.findItems(this.collection, newValue, this.objectKey);
-      if (selection.indexes) {
-        this.selectedValue = selection.indexes;
-      } else {
+      if (this.isEmptySelection(selection)) {
         this.selectedValue = (this.util.isObject(this.collection[0]) ? this.collection[0][this.objectKey] : this.collection[0]);
+        this.selectedItem = this.collection[0];
+      } else {
+        this.selectedValue = selection.index;
+        this.selectedItem = selection.item;
       }
-      this.selectedItem = selection.items ? selection.items : this.collection[0];
       this.renderSelection(selection);
     });
   }
@@ -453,9 +435,9 @@ export class AbpSelectCustomElement {
    */
   watchOnChangedToUpdateValueAndItemObjects() {
     this.domElm.on('changed.bs.select', (e, clickedIndex, newValue, oldValue) => {
-      this.selectedValue = this.domElm.selectpicker('val');
-      let selection = this.findItems(this.collection, this.selectedValue, this.objectKey);
-      this.selectedItem = selection.items;
+      const val = this.domElm.selectpicker('val');
+      let selection = this.findItems(this.collection, val, this.objectKey);
+      this.selectedValue = selection.index;
     });
   }
 } // class > end
