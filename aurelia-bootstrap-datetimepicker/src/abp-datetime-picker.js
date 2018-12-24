e52952ee3092ddbf103f1c1143436e3ccb9df7c8
ghiscoding <gbeaulac@gmail.com>
2018-12-24 12:15:00 -0500

    fixing another infinite loop problem, issue #54

--- aurelia-bootstrap-datetimepicker/src/abp-datetime-picker.js
+++ aurelia-bootstrap-datetimepicker/src/abp-datetime-picker.js
@@ -142,8 +142,12 @@ export class AbpDatetimePickerCustomElement {
     let value = this._originalValue || this._originalDateObject;
 
     if (value && moment(value, this._format, true).isValid()) {
-      this.model = moment(value, this._format, true).toDate();
-      this.value = moment(value, this._format, true).format(this._format);
+      if (!this.model) {
+        this.model = moment(value, this._format, true).toDate();
+      }
+      if (!this.value) {
+        this.value = moment(value, this._format, true);
+      }
     }
   }
 
@@ -306,12 +310,12 @@ export class AbpDatetimePickerCustomElement {
   }
 
   modelChanged(newValue, oldValue) {
-    if (!moment(newValue).isValid() && newValue !== null) {
+    if (!moment(newValue, this._format, true).isValid() && newValue !== null) {
       throw new Error('Datetimepicker, model.bind must be of type Date');
     }
     if (newValue !== oldValue && newValue) {
-      if (!oldValue || (moment(oldValue).isValid() && !moment(oldValue).isSame())) {
-        this.value = moment(newValue).format(this._format);
+      if (!oldValue || (!moment(newValue).isSame(oldValue))) {
+        this.value = moment(newValue, this._format, true).format(this._format);
       }
     }
   }
@@ -330,7 +334,7 @@ export class AbpDatetimePickerCustomElement {
   valueChanged(newValue, oldValue) {
     if (newValue !== oldValue && newValue) {
       if (moment(newValue, this._format, true).isValid()) {
-        if (!oldValue || (moment(oldValue, this._format, true).isValid() && !moment(oldValue, this._format, true).isSame())) {
+        if (!oldValue || (!moment(newValue, this._format, true).isSame(oldValue))) {
           this.model = moment(newValue, this._format, true).toDate();
         }
       }
